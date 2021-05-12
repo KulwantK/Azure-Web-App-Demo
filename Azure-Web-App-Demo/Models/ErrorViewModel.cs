7d32afba65b49a877e96ca66a6f08280fa44003a
Kulwant Kumar <kulwant.kumar@civica2.com>
2021-05-12 15:20:14 +0530

    Add project files.

---
+++ Azure-Web-App-Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure_Web_App_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
